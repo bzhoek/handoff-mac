4e45a87017fe0c7965fc8d3d2930ad6b87621afc
Bas van der Hoek <bas@hoek.com>
2019-09-09 13:36:10 +0200

    Initial Commit

---
+++ Classroom Timer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Classroom Timer
+//
+//  Created by Bas van der Hoek on 09/09/2019.
+//  Copyright © 2019 Zilverline. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
---
+++ Classroom TimerTests/Classroom_TimerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Classroom_TimerTests.swift
+//  Classroom TimerTests
+//
+//  Created by Bas van der Hoek on 09/09/2019.
+//  Copyright © 2019 Zilverline. All rights reserved.
+//
+
+import XCTest
+@testable import Classroom_Timer
+
+class Classroom_TimerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
